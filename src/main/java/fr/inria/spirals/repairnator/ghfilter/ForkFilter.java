16eae8a5433dcee8810e338e618aa7e8539b91e5
Simon Urli <simon.urli@inria.fr>
2018-05-17 16:55:36 +0200

    Improve script and push result

--- src/main/java/fr/inria/spirals/repairnator/ghfilter/ForkFilter.java
+++ src/main/java/fr/inria/spirals/repairnator/ghfilter/ForkFilter.java
@@ -1,5 +1,6 @@
 package fr.inria.spirals.repairnator.ghfilter;
 
+import org.kohsuke.github.GHRateLimit;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
@@ -16,13 +17,23 @@ public class ForkFilter {
 
     // this script intends to filter from a list the forks if the original repo is already on the list
     // or if another fork is already on the list
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String ghToken = args[0];
         String inputFilename = args[1];
         String outputFilename = args[2];
 
-        GitHub gitHub = new GitHubBuilder().withOAuthToken(ghToken).build();
-        int rateLimit = gitHub.getRateLimit().limit;
+        GitHub gitHub = null;
+        int rateLimit = 0;
+        GHRateLimit ghRateLimit = null;
+        try {
+            gitHub = new GitHubBuilder().withOAuthToken(ghToken).build();
+            ghRateLimit = gitHub.getRateLimit();
+            rateLimit = ghRateLimit.limit;
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Please check your github token.");
+            System.exit(1);
+        }
 
         if (rateLimit != 5000) {
             System.err.println("Please check your github token.");
@@ -42,24 +53,40 @@ public class ForkFilter {
         }
 
 
-        List<String> repoSlugs = Files.readAllLines(inputFile.toPath());
+        List<String> repoSlugs = null;
+        try {
+            repoSlugs = Files.readAllLines(inputFile.toPath());
+        } catch (IOException e) {
+            System.err.println("Error while reading the list of builds");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        if (ghRateLimit.remaining < repoSlugs.size()) {
+            System.err.println("You only have: "+ ghRateLimit.remaining + " authorized requests, which is less than the " + repoSlugs.size() + " directories to read. Stop now.");
+            System.exit(1);
+        }
 
         Set<GHRepository> parentRepositories = new HashSet<>();
         List<String> result = new ArrayList<>();
 
 
         for (String repoSlug : repoSlugs) {
-            GHRepository repository = gitHub.getRepository(repoSlug);
-
-            if (repository.isFork()) {
-                GHRepository parentRepo = repository.getParent();
-                if (!parentRepositories.contains(parentRepo)) {
-                    parentRepositories.add(parentRepo);
+            try {
+                GHRepository repository = gitHub.getRepository(repoSlug);
+                if (repository.isFork()) {
+                    GHRepository parentRepo = repository.getParent();
+                    if (!parentRepositories.contains(parentRepo)) {
+                        parentRepositories.add(parentRepo);
+                        result.add(repository.getFullName());
+                    }
+                } else {
+                    parentRepositories.add(repository);
                     result.add(repository.getFullName());
                 }
-            } else {
-                parentRepositories.add(repository);
-                result.add(repository.getFullName());
+            } catch (IOException e) {
+                System.err.println("Error while getting repository");
+                e.printStackTrace();
             }
         }
 
